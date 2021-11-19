34b1ff76d54abfd5b5afec1e6fc3fc2bbd28a41c
maanex <andreas@maanex.me>
2021-11-19 12:22:07 +0100

    xD

--- src/index.ts
+++ src/index.ts
@@ -172,8 +172,7 @@ function mountBot(shardIds: number[], shardCount: number) {
     ws: {
     },
     intents: [
-      'GUILDS',
-      'GUILD_MESSAGES'
+      'GUILDS'
     ],
     allowedMentions: {},
     makeCache: Options.cacheWithLimits({
