0f1d6bc8b18b97ffc3cc1becf3765a0ea43e1a82
maanex <andreas@maanex.me>
2022-05-22 14:18:56 +0200

    minifix

--- apps/api/src/routes/dash/admin/services.ts
+++ apps/api/src/routes/dash/admin/services.ts
@@ -5,9 +5,9 @@ import ReqError from '../../../lib/req-error'
 
 
 export async function getServices(req: Request, res: Response) {
-  const { status, data } = await axios.get(`/services/composed`, {
-    baseURL: config.network.manager
-  })
+  const { status, data } = await axios
+    .get(`/services/composed`, { baseURL: config.network.manager })
+    .catch(() => ({ status: 999, data: null }))
 
   if (status < 200 || status >= 300)
     return ReqError.badGateway(res)
