84a03a767a293297048f999fac0e7661690fbbb7
maanex <andreas@maanex.me>
2022-06-28 22:26:41 +0200

    debugging4

--- packages/common/src/lib/umi-libs.ts
+++ packages/common/src/lib/umi-libs.ts
@@ -70,9 +70,9 @@ export default class UmiLibs {
   public static ipLockMiddleware(range: string) {
     const subnet = range ? ip.cidrSubnet(range) : null
     return (req: Request, res: Response, next: NextFunction) => {
-      if (!range || subnet.contains(req.ip)) return next()
+      const reqIp = req.ip.replace('::ffff:', '').split('/')[0]
+      if (!range || subnet.contains(reqIp)) return next()
 
-      Logger.debug('TRIED TO ACCESS. IP ' + req.ip)
       res.status(407).send(`Not allowed. Your IP address does not have access to this resource.\n${req.ip}`)
     }
   }
