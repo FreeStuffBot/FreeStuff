5e99e87d579119f350267e75009e9df8bfbdf891
maanex <andreas@maanex.me>
2022-07-10 16:19:16 +0200

    oof5

--- apps/discord-publisher/src/tasks/discord-publish.ts
+++ apps/discord-publisher/src/tasks/discord-publish.ts
@@ -1,6 +1,5 @@
 import { Task, TaskId } from "@freestuffbot/rabbit-hole"
-import { DataWebhook, FSApiGateway, GuildDataType, GuildSanitizer, GuildType, Logger, ProductFilter, SanitizedGuildType, SanitizedProductType, Themes } from "@freestuffbot/common"
-import axios from "axios"
+import { FSApiGateway, GuildDataType, GuildSanitizer, ProductFilter, SanitizedProductType, Themes } from "@freestuffbot/common"
 import Mongo from "../database/mongo"
 import Upstream from "../lib/upstream"
 
@@ -12,7 +11,7 @@ export default async function handleDiscordPublish(task: Task<TaskId.DISCORD_PUB
 
   const query = {
     sharder: { $mod: [ bucketCount, bucketNumber ] },
-    // webhook: { $ne: null }
+    webhook: { $ne: null }
   }
 
   const guilds = await Mongo.Guild
@@ -48,49 +47,11 @@ async function sendToGuild(guild: GuildDataType, products: SanitizedProductType[
   if (sanitizedGuild.webhook)
     return true
 
-  await locateAndRegisterWebhook(sanitizedGuild)
-
-  // await Upstream.queueRequest({
-  //   method: 'POST',
-  //   url: `https://discord.com/api/webhooks/${sanitizedGuild.webhook}`,
-  //   data: theme
-  // })
-
-  return true
-}
-
-async function locateAndRegisterWebhook(guild: SanitizedGuildType) {
-  if (!guild.channel) return
-  const { data, status } = await axios.get(`http://discord_gateway/webhooks/${guild.channel.toString()}?softcache`, {
-    validateStatus: null
+  await Upstream.queueRequest({
+    method: 'POST',
+    url: `https://discord.com/api/webhooks/${sanitizedGuild.webhook}`,
+    data: theme
   })
 
-  if (status !== 200) {
-    Logger.debug(`Locate and register: status from gateway is ${status}`)
-    return
-  }
-
-  const hooks: DataWebhook[] = data
-  if (!hooks.length) {
-    Logger.debug(`Locate and register: no hooks`)
-    return
-  }
-
-  const webhook = `${hooks[0].id}/${hooks[0].token}`
-  // Mongo.Guild.updateOne({
-  //   _id: guild.id,
-  // }, {
-  //   $set: {
-  //     webhook
-  //   }
-  // })
-  const g = await Mongo.Guild.findById(guild.id) as GuildType
-  if (!g) {
-    Logger.debug(`Locate and register: guild not found. YIKES.`)
-    return
-  }
-  g.webhook = webhook
-  await g.save()
-
-  Logger.debug(`Locate and register: registered for ${guild.id.toString()} (${hooks.length})`)
+  return true
 }
