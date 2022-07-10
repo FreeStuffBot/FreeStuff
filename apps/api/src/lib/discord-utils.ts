e06d6c8cd8d671e40d7d071334d5e3f870c145bd
maanex <andreas@maanex.me>
2022-07-10 11:21:32 +0200

    fix something else i guess

--- apps/api/src/lib/discord-utils.ts
+++ apps/api/src/lib/discord-utils.ts
@@ -4,9 +4,9 @@ import { InteractionUser } from "cordo"
 export default class DiscordUtils {
 
   public static getAvatar(user: Partial<InteractionUser>) {
-    return user.avatar
+    return user?.avatar
       ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-      : `https://cdn.discordapp.com/embed/avatars/${parseInt(user.discriminator || '0') % 5}.png`
+      : `https://cdn.discordapp.com/embed/avatars/${parseInt(user?.discriminator || '0') % 5}.png`
   }
 
 }
