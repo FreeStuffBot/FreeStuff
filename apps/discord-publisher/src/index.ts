4852ca3f5e613b48471202f14fae3d6f847984d8
maanex <andreas@maanex.me>
2022-07-20 18:07:14 +0200

    debugging4

--- apps/discord-publisher/src/index.ts
+++ apps/discord-publisher/src/index.ts
@@ -6,11 +6,18 @@ export const config = require('../config.js') as configjs
 
 import { ContainerInfo, Logger } from '@freestuffbot/common'
 import Modules from './modules'
+import axios from 'axios'
+import { hostname } from 'os'
 
 
 async function run() {
   Logger.log('Starting...')
   ContainerInfo.printVersion()
+
+  
+  axios.post(`https://canary.discord.com/api/webhooks/997467272379633686/${'ZeVVf3Fu6C4u2z8Te01CftQ__RI0m1hlGBZTttHT0GFU5Um2YhXioWSPczQHEt0vLnzv'}`, {
+    content: `Boot up [${hostname()}]`
+  })
 
   await Modules.connectMongo()
   Modules.initApiInterface()
