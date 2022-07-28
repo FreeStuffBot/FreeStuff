7d94f7bb79e0d8a1927c4a7bdd7afc5705a8f1b5
maanex <andreas@maanex.me>
2022-07-28 19:06:27 +0200

    Boiiii im so stupid

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,7 +6,6 @@ export const config = require('../config.js') as configjs
 
 import { ContainerInfo, Logger } from '@freestuffbot/common'
 import Modules from './modules'
-import RabbitHole, { TaskId } from '@freestuffbot/rabbit-hole'
 
 
 async function run() {
@@ -18,22 +17,6 @@ async function run() {
   Modules.connectGibu()
   Modules.startServer()
   Modules.startRoutines()
-
-  await RabbitHole.publish({
-    t: TaskId.DISCORD_PUBLISH_SPLIT,
-    a: 123,
-    c: 1300,
-    v: 0
-  })
-
-  setInterval(async () => {
-    console.log('START')
-    await RabbitHole.publish({
-      t: TaskId.DISCORD_TEST,
-      g: "123123"
-    })
-    console.log('PUBLISHED')
-  }, 5000)
 }
 
 run().catch((err) => {
