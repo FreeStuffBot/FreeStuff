def3a143a811537e842264d5c692b4c3fd7d33f0
maanex <andreas@maanex.me>
2022-11-23 14:45:29 +0100

    errors please

--- apps/api/src/lib/discord-bridge.ts
+++ apps/api/src/lib/discord-bridge.ts
@@ -14,7 +14,7 @@ export class DiscordBridge {
   public static async assignRole(userid: string, ...addRoles: string[]): Promise<boolean> {
     const userRes = await axios
       .get(`/guilds/${guild}/members/${userid}`, opts)
-      .catch(() => null)
+      .catch(void console.error)
     if (userRes?.status !== 200) return false
 
     const roles = userRes.data.roles as string[]
@@ -27,7 +27,7 @@ export class DiscordBridge {
     const data = { roles }
     const postRes = await axios
       .patch(`/guilds/${guild}/members/${userid}`, data, opts)
-      .catch(() => null)
+      .catch(void console.error)
     return !!(postRes?.status === 200)
   }
 
