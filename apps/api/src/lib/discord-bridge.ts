0146c81be4a67411813eea20d9427e450fad6ed2
maanex <andreas@maanex.me>
2022-11-23 15:03:51 +0100

    owie

--- apps/api/src/lib/discord-bridge.ts
+++ apps/api/src/lib/discord-bridge.ts
@@ -1,19 +1,26 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 import { config } from ".."
-
 
 const baseURL = 'https://discord.com/api/v10'
 const guild = config.discordCommunity.guildId
 
-const headers = { Authorization: `Bot ${config.discordCommunity.helperAuthToken}` }
-const opts = { baseURL, headers, validateStatus: null }
-
 export class DiscordBridge {
+
+  private static buildOpts(additionalHeaders?: any): AxiosRequestConfig<any> {
+    return {
+      baseURL,
+      headers: {
+        ...additionalHeaders,
+        Authorization: `Bot ${config.discordCommunity.helperAuthToken}`
+      },
+      validateStatus: null
+    }
+  }
 
   /** true if successful */
   public static async assignRole(userid: string, ...addRoles: string[]): Promise<boolean> {
     const userRes = await axios
-      .get(`/guilds/${guild}/members/${userid}`, opts)
+      .get(`/guilds/${guild}/members/${userid}`, this.buildOpts())
       .catch(() => null)
     if (userRes?.status !== 200) console.error(userRes)
     if (userRes?.status !== 200) return false
@@ -27,7 +34,7 @@ export class DiscordBridge {
 
     const data = { roles }
     const postRes = await axios
-      .patch(`/guilds/${guild}/members/${userid}`, data, opts)
+      .patch(`/guilds/${guild}/members/${userid}`, data, this.buildOpts())
       .catch(() => null)
     if (postRes?.status !== 200) console.error(postRes)
     return !!(postRes?.status === 200)
