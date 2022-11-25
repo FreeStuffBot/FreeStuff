f9073907822fe9128fd3288b2453f8c1f133e2c5
Rami Sabbagh <rami.sab07@gmail.com>
2022-11-25 22:37:25 +0300

    Implement the Telegram publisher

--- apps/telegram-publisher/src/tasks/telegram-publish-split.ts
+++ apps/telegram-publisher/src/tasks/telegram-publish-split.ts
@@ -1,8 +1,53 @@
-import { Task, TaskId } from "@freestuffbot/rabbit-hole"
+import { CMS, Logger } from '@freestuffbot/common'
+import RabbitHole, { Task, TaskId } from "@freestuffbot/rabbit-hole"
 
+/*
+PUBLISH_SPLIT is triggered a single time by the system when it's a new announcement is published.
+  (You can find it in apps/api/src/lib/upstream.ts -> publishToTelegram method).
 
-export default function handleAppsPublishSplit(task: Task<TaskId.TELEGRAM_PUBLISH_SPLIT>): Promise<boolean> {
-  // TODO handle task, return if successful
+The "announcementBucketCount" is hardcoded to 1. (which is the 'c' field of the task).
+The 'a' field is the announcement id, query the CMS to get the full product details.
+The 'v' field is fixed to 0.
 
-  return Promise.resolve(true)
+It's expected from this implementation to queue the subsequent PUBLISH_SPLIT tasks,
+and when that is done, due to the priorities of tasks consumption,
+all the queued PUBLISH tasks are consumed before the next PUBLISH_SPLIT one is consumed.
+
+Check packages/rabbit-hole/src/types/tasks.ts to figure out the structure of the task object.
+*/
+
+export default async function handleAppsPublishSplit(task: Task<TaskId.TELEGRAM_PUBLISH_SPLIT>): Promise<boolean> {
+  const targetChannels = CMS.remoteConfig[1]?.global?.telegramChannels;
+  if (targetChannels === undefined) {
+    Logger.warn('No Telegram channels are configured, skipping announcements.');
+    return true;
+  }
+
+  const totalBuckets = task.c;
+  const totalChannels = targetChannels.length;
+
+  const announcementId = task.a;
+
+  const firstChannelIndex = task.v;
+  const lastChannelIndex = Math.min(firstChannelIndex + Math.ceil(totalChannels / totalBuckets), totalChannels); // (excluded)
+
+  for (let channelIndex=firstChannelIndex; channelIndex < lastChannelIndex; channelIndex++) {
+    await RabbitHole.publish({
+      t: TaskId.TELEGRAM_PUBLISH,
+      a: announcementId,
+      b: channelIndex,
+      c: totalBuckets,
+    })
+  }
+
+  if (lastChannelIndex !== totalChannels) {
+    await RabbitHole.publish({
+      t: TaskId.TELEGRAM_PUBLISH_SPLIT,
+      a: announcementId,
+      c: totalBuckets,
+      v: lastChannelIndex,
+    });
+  }
+
+  return true;
 }
--- apps/telegram-publisher/src/tasks/telegram-publish.ts
+++ apps/telegram-publisher/src/tasks/telegram-publish.ts
@@ -1,8 +1,24 @@
+import { CMS, FSApiGateway, Logger } from '@freestuffbot/common';
 import { Task, TaskId } from "@freestuffbot/rabbit-hole"
+import { sendProduct } from '../lib/message-formatter';
 
 
 export default async function handleAppsPublish(task: Task<TaskId.TELEGRAM_PUBLISH>): Promise<boolean> {
-  // TODO handle task, return if successful
+  const targetChannels = CMS.remoteConfig[ 1 ]?.global?.telegramChannels;
+  if (targetChannels === undefined) {
+    Logger.warn('Failed to load telegram channels configuration');
+    return false;
+  }
 
-  return true
+  const announcementId = task.a;
+  const channelIndex = task.b;
+
+  const { chatId, locale } = targetChannels[ channelIndex ];
+
+  if (!chatId) throw new Error('Invalid bucket number: ' + chatId);
+
+  const products = await FSApiGateway.getProductsForAnnouncement(announcementId);
+  for (const product of products) await sendProduct(chatId, product, locale);
+
+  return true;
 }
