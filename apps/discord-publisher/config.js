7af47d6c05c29dd45e03a20b4502087745a8180d
Andreas <andreas@maanex.me>
2022-12-25 10:11:25 +0000

    faster!!!!

--- apps/discord-publisher/config.js
+++ apps/discord-publisher/config.js
@@ -1,6 +1,7 @@
 const loadArg = require('@freestuffbot/config/load-arg')
 
 
+/** @type {import('./src/types/config').configjs} */
 module.exports = {
   port: Number(process.argv[2]) || loadArg('DISCORD_PUBLISHER_PORT') || 80,
   mongoUrl: loadArg('DISCORD_PUBLISHER_MONGO_URL'),
@@ -9,14 +10,14 @@ module.exports = {
     umiAllowedIpRange: loadArg('NETWORK_UMI_ALLOWED_IP_RANGE')
   },
   behavior: {
-    upstreamRequestRate: 15, // TODO up this to 10 once running stable
+    upstreamRequestRate: 30,
     upstreamRequestInterval: 250,
     upstreamMaxRetries: 10,
-    upstreamMaxFramesInQueue: 5,
-    upstreamMaxPendingReplyCount: 50,
+    upstreamMaxFramesInQueue: 6,
+    upstreamMaxPendingReplyCount: 70,
     upstreamClientErrorsTimeframeMinutes: 10,
     upstreamClientErrorsMax: 10000,
-    upstreamClientErrorActionLeeway: 0.9,
+    upstreamClientErrorActionLeeway: 0.6,
     publishSplitTaskAmount: 20,
     publishTaskBatchSize: 10
   },
